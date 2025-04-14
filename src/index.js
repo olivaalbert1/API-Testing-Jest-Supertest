869f0caafb070679b6acc4d1dfa2f71e258a2f12
Albert Oliva <oliva.albert+1@gmail.com>
2025-04-14 16:50:27 +0200

    server up&running

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+}
+);
+
+// Export the app for testing purposes
+export default app;
