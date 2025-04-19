e03800b541e4452ec29ad0581a410510350f8e06
Albert Oliva <oliva.albert+1@gmail.com>
2025-04-19 17:16:41 +0200

    fix repeated report upload

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
             'jest-html-reporters',
             {
                 publicPath: './Report', // represents the path to the report directory
-                filename: 'sTReport.html', // report file name
+                filename: 'report.html', // report file name
                 pageTitle: 'SuperTest and Jest API Test Report', // report title
                 overwrite: true, // overwrite existing file
                 expand: true, // expands the report directory
